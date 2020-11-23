977a22dc8f708a17f3d67240ac3ddc0db653f0dd
Chris Gibb <chris.gibb@outlook.com>
2020-11-22 23:09:53 -0500

    write version before publishing with npm

--- lib/publishPreRelease.dart
+++ lib/publishPreRelease.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:meta/meta.dart';
 import 'package:version/version.dart';
 
 Future<void> publishPreRelease({@required Version version}) async {
+  var package = json.decode(File("package.json").readAsStringSync());
+  package["version"] = version.toString();
+
+  File("package.json").writeAsStringSync(json.encode(package));
+
   await Process.run("npm", [
     "publish",
     "--tag",
