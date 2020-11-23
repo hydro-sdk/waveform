49da3e9404a37122ecefb5ee4dd3bb3f784b2cea
Chris Gibb <chris.gibb@outlook.com>
2020-11-22 23:33:27 -0500

    fix whitespace split when reading git tags

--- lib/readTags.dart
+++ lib/readTags.dart
@@ -3,7 +3,7 @@ import 'package:git/git.dart';
 Future<List<String>> readTags() async {
   var res = await runGit(["tag", "--column"]);
 
-  var lines = (res.stdout as String)?.split("\s");
+  var lines = (res.stdout as String)?.split(RegExp(" "));
   lines.removeWhere((x) => x.isEmpty);
   lines.removeWhere((x) => x.trim().isEmpty);
 
