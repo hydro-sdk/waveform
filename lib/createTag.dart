14e9cd0ba004b15f1f5f95f218844111060a4751
Chris Gibb <chris.gibb@outlook.com>
2020-11-22 23:09:32 -0500

    use git to create tags

--- lib/createTag.dart
+++ lib/createTag.dart
@@ -1,11 +1,8 @@
-import 'dart:io';
-
+import 'package:git/git.dart';
 import 'package:meta/meta.dart';
 import 'package:version/version.dart';
 
 Future<void> createTag({@required Version version}) async {
-  await Process.run("npm", ["version", version.toString()]).then((process) {
-    print(process.stdout);
-    print(process.stderr);
-  });
+  await runGit(
+      ["tag", "-a", version.toString(), "-m", "\"${version.toString()}\""]);
 }
