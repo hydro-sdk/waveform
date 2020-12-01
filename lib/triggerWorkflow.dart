4ab426ab31c062b92bef5ae2df4caa1f96244212
Chris Gibb <chris.gibb@outlook.com>
2020-11-30 20:26:17 -0500

    add request logging

--- lib/triggerWorkflow.dart
+++ lib/triggerWorkflow.dart
@@ -8,13 +8,17 @@ Future<void> triggerWorkflow({
   @required String ref,
   @required String dependencyName,
   @required Version dependencyVersion,
-}) async =>
-    post(
-        "https://github.com/repos/hydro-sdk/$repoName/actions/workflows/waveform-upgrade/dispatches",
-        headers: {
-          "Authorization": "token $authToken",
-          "Accept": "application/vnd.github.v3+json",
-        },
-        body: """
+}) async {
+  var res = await post(
+      "https://github.com/repos/hydro-sdk/$repoName/actions/workflows/waveform-upgrade/dispatches",
+      headers: {
+        "Authorization": "token $authToken",
+        "Accept": "application/vnd.github.v3+json",
+      },
+      body: """
        {"ref":"$ref", "inputs": {"depName": "@hydro-sdk/$dependencyName","depVersion":"$dependencyVersion" }}
         """);
+
+  print(res.statusCode);
+  print(res.body);
+}
