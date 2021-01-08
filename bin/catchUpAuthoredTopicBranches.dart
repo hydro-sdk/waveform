0876fc044ef1eb9fd4b1ab29836f9a176ed9b5b8
Chris Gibb <chris.gibb@outlook.com>
2021-01-07 23:09:55 -0500

    drop status checks

--- bin/catchUpAuthoredTopicBranches.dart
+++ bin/catchUpAuthoredTopicBranches.dart
@@ -1,7 +1,6 @@
 import 'package:args/args.dart';
 import 'package:github/github.dart';
 import 'package:waveform/catchupBranch.dart';
-import 'package:waveform/checksPassing.dart';
 
 void main(List<String> args) async {
   var parser = ArgParser();
@@ -21,18 +20,10 @@ void main(List<String> args) async {
       .toList();
 
   for (var pr in prs) {
-    var passing = await checksPassing(
-      prNum: pr.number.toString(),
-    );
-
-    print("Status checks for ${pr.id} ${passing ? "passing" : "failing"}");
-
-    if (passing) {
       print("Merging the latest from ${pr.base.ref} into ${pr.head.ref}");
       await catchupBranch(
         baseBranchName: pr.base.ref,
         headBranchName: pr.head.ref,
       );
-    }
   }
 }
