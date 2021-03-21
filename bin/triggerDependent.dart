6354847778810c01ff159688f0a31489099c3c3b
Chris Gibb <chris.gibb@outlook.com>
2021-03-20 21:17:40 -0400

    take version to send updates to from cli instead of from package json

--- bin/triggerDependent.dart
+++ bin/triggerDependent.dart
@@ -1,11 +1,9 @@
 import 'package:args/args.dart';
 import 'package:version/version.dart';
 import 'package:waveform/readPackageNameFromPackage.dart';
-import 'package:waveform/readVersionFromPackage.dart';
 import 'package:waveform/triggerWorkflow.dart';
 
 void main(List<String> args) async {
-  var version = Version.parse(readVersionFromPackage());
   var name = readPackageNameFromPackage();
 
   var parser = ArgParser();
@@ -13,17 +11,18 @@ void main(List<String> args) async {
   parser.addOption("token");
   parser.addOption("repo");
   parser.addOption("ref");
+  parser.addOption("version");
 
   var results = parser.parse(args);
 
   print(
-      "Triggering update workflow to update $name to $version on ${results["repo"]}");
+      "Triggering update workflow to update $name to ${results["version"]} on ${results["repo"]}");
 
   await triggerWorkflow(
     authToken: results["token"],
     repoName: results["repo"],
     ref: results["ref"],
     dependencyName: name,
-    dependencyVersion: version,
+    dependencyVersion: Version.parse(results["version"]),
   );
 }
